e7e870899cd774a5c6f80bcc2cef005e8727d1ed
EslamNady <esspk01@gmail.com>
2019-07-17 16:47:40 +0200

    V8

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 
 function sliderheight() {
-  var divHeight = $('.signup-page').height();
+  var divHeight = $('.signup-page.active').height();
   $('.signup-pages-wrapper').css({ 'height': divHeight });
+
+
+
 }
 sliderheight();
 $(window).resize(sliderheight);
 
+function sliderheight2() {
+  var divHeight2 = $('.new-trip-page.active').height();
+  $('.new-trip-pages-wrapper').css({ 'height': divHeight2 });
+}
+$('.nav-link').click(function () {
+  setTimeout(function () {
+    sliderheight2();
+  }, 200)
+
+})
+sliderheight2();
+$(window).resize(sliderheight2);
 
 // Get the navbar
 var navbar = document.getElementById("primary-nav");
@@ -39,6 +54,12 @@ function signupCardSwip() {
             $(this).css({ 'border-color': 'red' })
           }
         }
+        if ($(this).attr('name') == 'phone') {
+          if (!isNumber($(this).val())) {
+            allowSwip = isNumber($(this).val())
+            $(this).css({ 'border-color': 'red' })
+          }
+        }
       }
     });
 
@@ -49,7 +70,6 @@ function signupCardSwip() {
       $('.prev-btn').removeClass('d-none');
 
       const activeCard = $(".signup-page.active");
-      // console.log(activeCard)
       const currentCardNum = activeCard.index();
       if (currentCardNum + 1 == 2) {
         $('#next-btn').addClass('d-none');
@@ -71,9 +91,10 @@ function signupCardSwip() {
       $('.signup-page').eq(currentCardNum + 1).addClass('active')
 
 
+
     }
 
-
+    sliderheight();
 
   });
   $('.prev-btn').click(function () {
@@ -97,10 +118,76 @@ function signupCardSwip() {
 
     $('.signup-page').eq(activeCardNum - 1).removeClass('prev')
     $('.signup-page').eq(activeCardNum - 1).addClass('active')
-
+    sliderheight();
   });
+  sliderheight();
 }
 signupCardSwip()
+
+function newTripCardSwap() {
+  $('.next-btn').click(function () {
+    const activeCard = $(".new-trip-page.active");
+    const currentCardNum = activeCard.index();
+    if (currentCardNum + 1 == 3) {
+      $('#next-btn').addClass('d-none');
+      $('#next-btn').removeClass('d-inline-block');
+      $('#submit-btn').removeClass('d-none');
+      $('#submit-btn').addClass('d-inline-block');
+    }
+    
+    activeCard.removeClass('active')
+    activeCard.addClass('prev')
+
+    $('.new-trip-page').eq(currentCardNum + 1).removeClass('next')
+    $('.new-trip-page').eq(currentCardNum + 1).removeClass('prev')
+    $('.new-trip-page').eq(currentCardNum + 1).addClass('active')
+
+    var currentStep = $('.new-trip-page.active').attr('step');
+    $('.new-trip-tabs').find('.active').removeClass('active');
+    $(`#tab-${currentStep}`).addClass('active');
+    sliderheight2();
+    $('.current-step').empty()
+    $('.current-step').append(currentStep)
+  });
+
+  $('.new-trip-tab').click(function () {
+    const target = $(this).attr('step');
+    const current = $('.new-trip-tab.active').attr('step');
+    $('.new-trip-tab.active').removeClass('active');
+    $(this).addClass('active');
+
+    $(`#page-${target}`).addClass('active');
+    $(`#page-${target}`).removeClass('next');
+    $(`#page-${target}`).removeClass('prev');
+
+    $(`#page-${current}`).removeClass('active');
+    if (target < current) {
+      $(`#page-${current}`).addClass('next');
+    } else if (target > current) {
+      $(`#page-${current}`).addClass('prev');
+    }
+
+    const activeCard = $(".new-trip-page.active");
+    const currentCardNum = activeCard.index();
+    if (currentCardNum + 1 == 4) {
+      $('#next-btn').addClass('d-none');
+      $('#next-btn').removeClass('d-inline-block');
+      $('#submit-btn').removeClass('d-none');
+      $('#submit-btn').addClass('d-inline-block');
+    }else{
+      $('#submit-btn').addClass('d-none');
+      $('#submit-btn').removeClass('d-inline-block');
+      $('#next-btn').removeClass('d-none');
+      $('#next-btn').addClass('d-inline-block');
+    }
+    
+    $('.current-step').empty()
+    $('.current-step').append(target)
+  });
+
+
+}
+newTripCardSwap()
 
 function fileInputChange() {
   $('.file-input').change(function () {
@@ -161,6 +248,9 @@ function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+function isNumber(str) {
+  return !/\D/.test(str);
+}
 function calender() {
   var events = [
     { "date": "2019-07-07", "badge": false, "title": "Example 1" },
@@ -212,3 +302,24 @@ if ($('#calendar-container').length > 0) {
   calender()
 }
 
+function showPayment() {
+  $('.white-card-body').click(function () {
+
+    const tripsWrapper = $(this).parents('.trips-wrapper');
+
+    //for ajax requests
+    const tripID = $(this).attr('tripID');
+    const isFinishedTrips = tripsWrapper.hasClass('finished-trips')
+
+    tripsWrapper.find('.cards-wrapper').addClass('d-none')
+    tripsWrapper.find('.payment-table-wrapper').removeClass('d-none')
+  });
+  $('.table-back-btn').click(function () {
+    const tableWrapper = $(this).parents('.payment-table-wrapper')
+
+    tableWrapper.addClass('d-none');
+    tableWrapper.siblings('.cards-wrapper').removeClass('d-none')
+
+  });
+}
+showPayment()
